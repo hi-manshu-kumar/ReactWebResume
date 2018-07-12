2daafc98b12ae68178cdc94d1383ca46268666f5
hi-manshu-kumar <himanshu.kumar394@gmail.com>
2018-07-12 18:12:56 +0530

    Added react structure

---
+++ react_web_resume/src/childs/header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+export const PageHeader = () => {
+    return (
+        <div>
+            <p>
+                I am header
+                <hr/>
+            </p>
+        </div>
+    )
+}
